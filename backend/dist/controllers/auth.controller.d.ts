70931284cec3b488a21a51baf6dd1a4170bb7374
suraj-ufx <suraj@unifyxperts.com>
2026-03-16 14:57:45 +0530

    removing dist folder

--- backend/dist/controllers/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare class AuthController {
-    private authService;
-    constructor();
-    register(req: Request, res: Response): Promise<void>;
-}
-//# sourceMappingURL=auth.controller.d.ts.map
--- backend/dist/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { AuthController } from '../controllers/auth.controller.js';
-const router = Router();
-const authcontroller = new AuthController();
-router.post('/auth/register', authcontroller.register);
-export default router;
-//# sourceMappingURL=auth.route.js.map
